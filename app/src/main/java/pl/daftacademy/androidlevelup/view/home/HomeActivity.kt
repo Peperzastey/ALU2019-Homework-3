ae754b2e7c46e2de9b6602ef91b7bd5452a36cdf
Przemyslaw Poljanski <peperzasty@gmail.com>
2019-03-26 21:32:45 +0100

    Add ActionBar title updating and fetching movie categories from string resources.

--- app/src/main/java/pl/daftacademy/androidlevelup/view/home/HomeActivity.kt
+++ app/src/main/java/pl/daftacademy/androidlevelup/view/home/HomeActivity.kt
@@ -10,17 +10,32 @@ import kotlinx.android.synthetic.main.activity_home.*
 import pl.daftacademy.androidlevelup.view.movies.MoviesFragment
 
 class HomeActivity : AppCompatActivity() {
+    // movie category strings
+    lateinit var ALL_MOVIES: String
+    lateinit var ACTION_MOVIES: String
+    lateinit var COMEDY_MOVIES: String
+    lateinit var CRIME_MOVIES: String
+    lateinit var HORROR_MOVIES: String
+    lateinit var ROMANCE_MOVIES: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        nav.setNavigationItemSelectedListener { changeFilter(it) }   // [this] ::changeFilter ?
-        if (savedInstanceState == null) filterByCategory(null)
         setSupportActionBar(toolbar)
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_menu)
         }
+
+        ALL_MOVIES = getString(R.string.menu_all_movies)
+        ACTION_MOVIES = getString(R.string.menu_action_movies)
+        COMEDY_MOVIES = getString(R.string.menu_comedy_movies)
+        CRIME_MOVIES = getString(R.string.menu_crime_movies)
+        HORROR_MOVIES = getString(R.string.menu_horror_movies)
+        ROMANCE_MOVIES = getString(R.string.menu_romance_movies)
+
+        nav.setNavigationItemSelectedListener { changeFilter(it) }   // [this] ::changeFilter ?
+        if (savedInstanceState == null) filterByCategory("All Movies")
     }
 
     override fun onOptionsItemSelected(item: MenuItem) =
@@ -35,12 +50,12 @@ class HomeActivity : AppCompatActivity() {
     private fun changeFilter(item: MenuItem): Boolean {
         //check Kotlin function: TODO("Implement")
         when (item.itemId) {
-            R.id.category_all_movies -> filterByCategory(null)
-            R.id.category_action_movies -> filterByCategory("Action")
-            R.id.category_comedy_movies -> filterByCategory("Comedy")
-            R.id.category_crime_movies -> filterByCategory("Crime")
-            R.id.category_horror_movies -> filterByCategory("Horror")
-            R.id.category_romance_movies -> filterByCategory("Romance")
+            R.id.category_all_movies -> filterByCategory(ALL_MOVIES)
+            R.id.category_action_movies -> filterByCategory(ACTION_MOVIES)
+            R.id.category_comedy_movies -> filterByCategory(COMEDY_MOVIES)
+            R.id.category_crime_movies -> filterByCategory(CRIME_MOVIES)
+            R.id.category_horror_movies -> filterByCategory(HORROR_MOVIES)
+            R.id.category_romance_movies -> filterByCategory(ROMANCE_MOVIES)
             else -> return false
         }
         nav.menu.children.find { it.isChecked }?.isChecked = false
@@ -49,10 +64,19 @@ class HomeActivity : AppCompatActivity() {
         return true
     }
 
-    private fun filterByCategory(name: String?/*, addToBackStack: Boolean = false*/) {
+    private fun filterByCategory(name: String) {
+        updateActionBarTitle(name)
+
+        val filter: String? = if (name == ALL_MOVIES) null else name
+
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.container, MoviesFragment.create(name))
+            .replace(R.id.container, MoviesFragment.create(filter))
             .commit()
+    }
+
+    private fun updateActionBarTitle(title: CharSequence) {
+        supportActionBar?.title = title //check null = title ??
+        //TODO check without toolbar in layout
     }
 }
